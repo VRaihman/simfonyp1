9a801d5909efee90c35347d9de4819e5d26c92b3
VRaihman <admin@crm.com>
2019-03-15 03:12:54 +0400

    fix6

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,13 +2,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\Validator\Validation;
 use Symfony\Component\Validator\Constraints as Assert;
 use App\Entity\DbProduct;
 use App\Entity\DbCart;
